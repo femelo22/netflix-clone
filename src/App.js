3fa6ed5902c94c09796c036ab988876c03d790be
luiz fernando <luiz123jfmg@gmail.com>
2021-10-05 19:59:48 -0300

    Retornando filme em dastaque

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Tmdb from './api/Tmdb'
 import MovieRow from './components/MovieRow';
 import FeatureMovie from './components/FeaturedMovie'
@@ -10,35 +10,34 @@ export default () => {
   const [featuredData, setFeacturedData] = useState(null);
 
   useEffect(() => {
-    const loadAll = async () => {
-      //Pegando a lista TOTAL
-      let list = await Tmdb.getHomeList();
-      setMovieList(list);
-
-      //console.log(list)
-
-      //Pegando o filme em destaque
-      let originals = list.filter(i=>i.slug === 'originals'); //filtra a lista com os filmes que possuem o slug = originals
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
-      let chosen = originals[0].items.results[randomChosen];
-
-      console.log(chosen)
-    } 
     loadAll();
   }, []);
+
+
+  const loadAll = async () => {
+    //Pegando a lista TOTAL
+    let list = await Tmdb.getHomeList();
+    //Pegando o filme em destaque
+    let originals = list.filter(i => i.slug === 'originals'); //filtra a lista com os filmes que possuem o slug = originals
+    let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+    let chosen = originals[0].items.results[randomChosen];
+    let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+    setMovieList(list);
+    setFeacturedData(chosenInfo);
+  }
 
   return (
     <div className="page">
 
-    {
-      featuredData && 
-      <FeatureMovie item={featuredData}/>
-    }
+      {
+        featuredData &&
+        <FeatureMovie item={featuredData} />
+      }
 
       <section className="lists">
         {movieList.map((item, key) => (
           <div>
-            <MovieRow key={key} title={item.title} items={item.items}/>
+            <MovieRow key={key} title={item.title} items={item.items} />
           </div>
         ))}
       </section>
--- src/api/Tmdb.js
+++ src/api/Tmdb.js
@@ -64,9 +64,23 @@ export default {
         ]
     },
 
-    getMovieInfo: async () => {
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
 
-        
+        if(movieId){
+            switch(type) {
+                case 'movie':
+                    info = await baseFetch(`/movie/${movieId}/?language=pt-BR&api_key=${API_KEY}`)
+                break;
+                case 'tv':
+                    info = await baseFetch(`/tv/${movieId}/?language=pt-BR&api_key=${API_KEY}`)
+                break;
+                default:
+                    info = null;
+                break;
+            }
+        }
+        return info;
     }
 
 
